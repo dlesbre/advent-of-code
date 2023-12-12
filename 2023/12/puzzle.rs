7f30d75594e136a493b7c0f244df6aaa52090294
Dorian Lesbre <dorian.lesbre@gmail.com>
2023-12-12 14:21:42 +0100

    Day 12 Part 2

--- 2023/12/puzzle.rs
+++ 2023/12/puzzle.rs
@@ -1,6 +1,8 @@
 // ==== Puzzle 12 : https://adventofcode.com/2023/day/12 ====
 
-#[derive(Clone, Copy)]
+// use std::collections::HashMap;
+
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum SpringStatus {
     Damaged,
     Operational,
@@ -20,46 +22,94 @@ fn parse_spring(c: char) -> SpringStatus {
 
 struct Line {
     springs: Vec<SpringStatus>,
-    info: Vec<usize>,
+    chunks: Vec<usize>,
 }
 
-struct Precomputed<'a> {
-    line: &'a Line,
-    max_from_here: Vec<usize>, // max damaged section starting at position,
-    remaining_total: Vec<usize>, // remaining total of damage section
-}
-
-fn precompute<'a>(line: &'a Line) -> Precomputed<'a> {
-    let mut remaining_total = line.info.clone();
-    for i in (0..(line.info.len() - 1)).rev() {
-        remaining_total[i] += 1 + remaining_total[i + 1];
+/// previous[i] = number of ways to fit chunks 0..n in line[0..i]
+/// Sets next[i] = number of ways to fit chunks 0..(n+1) in line[0..i],
+/// (taken as argument to avoid reallocating a new array)
+/// assumes: chunk is chunk[n]
+/// assumes: previous and next have size springs.len() + 1
+/// assumes: n > 0
+fn dynamic_next(
+    springs: &Vec<SpringStatus>,
+    previous: &Vec<usize>,
+    next: &mut Vec<usize>,
+    chunk: usize,
+) {
+    for i in 0..(chunk + 1) {
+        next[i] = 0; // can't fit chunk here
     }
-    let mut max_from_here: Vec<usize> = line.springs.iter().map(|_| 0).collect();
-    let mut next = 0;
-    for (i, n) in line.springs.iter().enumerate().rev() {
-        match n {
-            Operational => (),
-            Damaged | Unknown => max_from_here[i] += 1 + next,
+    for i in (chunk + 1)..next.len() {
+        match springs[i - 1] {
+            Operational => next[i] = next[i - 1],
+            Damaged | Unknown => {
+                // space before current segment
+                let space = i - chunk - 1;
+                if springs[space] != Damaged
+                    && springs[(i - chunk)..i].iter().all(|c| *c != Operational)
+                {
+                    next[i] = previous[i - chunk - 1]
+                } else {
+                    next[i] = 0;
+                }
+                // case Unkown is Operational
+                if springs[i - 1] == Unknown {
+                    next[i] += next[i - 1];
+                }
+            }
         }
-        next = max_from_here[i];
     }
-    Precomputed {
-        line,
-        remaining_total,
-        max_from_here,
+}
+
+/// Very similar do dynamic_next, but simpler for first chunk
+/// (no need for a leading space, no previous cases)
+fn dynamic_init(springs: &Vec<SpringStatus>, chunk: usize) -> Vec<usize> {
+    let mut next = Vec::new();
+    for _ in 0..chunk {
+        next.push(0);
     }
+    for i in chunk..(springs.len() + 1) {
+        match springs[i - 1] {
+            Operational => next.push(next[i - 1]),
+            Damaged | Unknown => {
+                // block can't be placed after first damaged
+                if springs[(i - chunk)..i].iter().all(|c| *c != Operational)
+                    && springs[0..(i - chunk)].iter().all(|c| *c != Damaged)
+                {
+                    next.push(1);
+                } else {
+                    next.push(0);
+                }
+                if springs[i - 1] == Unknown {
+                    next[i] += next[i - 1];
+                }
+            }
+        }
+    }
+    next
+}
+
+fn dynamic(line: &Line) -> usize {
+    let mut prev = dynamic_init(&line.springs, line.chunks[0]);
+    let mut next = prev.clone();
+    for chunk in line.chunks[1..].iter() {
+        dynamic_next(&line.springs, &prev, &mut next, *chunk);
+        std::mem::swap(&mut prev, &mut next);
+    }
+    *prev.last().unwrap()
 }
 
 fn parse_line(line: String) -> Line {
     let mut s = line.split(" ");
     let springs = s.next().unwrap().chars().map(parse_spring).collect();
-    let info = s
+    let chunks = s
         .next()
         .unwrap()
         .split(",")
         .filter_map(|c| c.parse().ok())
         .collect();
-    Line { springs, info }
+    Line { springs, chunks }
 }
 
 fn read_lines() -> Vec<Line> {
@@ -72,96 +122,20 @@ fn read_lines() -> Vec<Line> {
 
 fn extend(line: &Line) -> Line {
     let mut springs = line.springs.clone();
-    let mut info = line.info.clone();
+    let mut chunks = line.chunks.clone();
     for _ in 0..4 {
         springs.push(Unknown);
         springs.append(&mut line.springs.clone());
-        info.append(&mut line.info.clone());
+        chunks.append(&mut line.chunks.clone());
     }
-    Line { springs, info }
-}
-
-fn nb_damaged(p: &Precomputed, index_springs: usize, index_info: usize, damaged: usize) -> usize {
-    if index_springs == p.line.springs.len() {
-        if damaged == 0 && index_info == p.line.info.len() {
-            return 1;
-        }
-        return 0;
-    }
-    if damaged == 0 {
-        return match p.line.springs[index_springs] {
-            Damaged => 0,
-            Operational | Unknown => nb_undamaged(p, index_springs + 1, index_info),
-        };
-    }
-    match p.line.springs[index_springs] {
-        Operational => 0,
-        Damaged | Unknown => nb_damaged(p, index_springs + 1, index_info, damaged - 1),
-    }
-}
-
-fn nb_undamaged(p: &Precomputed, index_springs: usize, index_info: usize) -> usize {
-    if index_springs == p.line.springs.len() {
-        return if index_info == p.line.info.len() {
-            1
-        } else {
-            0
-        };
-    }
-    if index_info == p.line.info.len() {
-        return match p.line.springs[index_springs] {
-            Damaged => 0,
-            Operational | Unknown => nb_undamaged(p, index_springs + 1, index_info),
-        };
-    }
-    if p.remaining_total[index_info] > p.line.springs.len() - index_springs {
-        return 0;
-    }
-    // Not enough space to fit next segment of damaged before an undamaged spring
-    if p.max_from_here[index_springs] < p.line.info[index_info] {
-        // skip whole segment
-        return nb_undamaged(
-            p,
-            index_springs + p.max_from_here[index_springs] + 1,
-            index_info,
-        );
-    }
-    match p.line.springs[index_springs] {
-        Operational => nb_undamaged(p, index_springs + 1, index_info),
-        Damaged => nb_damaged(
-            p,
-            index_springs + 1,
-            index_info + 1,
-            p.line.info[index_info] - 1,
-        ),
-        Unknown => {
-            let u = nb_undamaged(p, index_springs + 1, index_info);
-            let v = nb_damaged(
-                p,
-                index_springs + 1,
-                index_info + 1,
-                p.line.info[index_info] - 1,
-            );
-            u + v
-        }
-    }
-}
-
-fn solve(line: &Line) -> usize {
-    nb_undamaged(&precompute(line), 0, 0)
+    Line { springs, chunks }
 }
 
 fn main() {
     let input = read_lines();
-    let p1: usize = input.iter().map(solve).sum();
+    let p1: usize = input.iter().map(dynamic).sum();
     println!("Part 1 : {}", p1);
 
-    let p2: usize = input
-        .iter()
-        .map(|l| {
-            println!("Done!");
-            solve(&extend(l))
-        })
-        .sum();
+    let p2: usize = input.iter().map(|l| dynamic(&extend(l))).sum();
     println!("Part 2 : {}", p2);
 }
