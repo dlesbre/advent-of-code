66e85467a0770657c7545fd636ce9ea981af0a18
Dorian Lesbre <dorian.lesbre@gmail.com>
2024-12-22 13:26:05 +0100

    Uses bytes and a single int for 4-digit sequence (0.9s)

--- 2024/Day22/puzzle.ml
+++ 2024/Day22/puzzle.ml
@@ -14,25 +14,36 @@ let rec iter number = function
 
 let part1 = list_sum (fun n -> iter n 2000)
 
+let get b n = int_of_char (Bytes.unsafe_get b n)
+
 let prices number =
   let nb = ref number in
-  let prices = Array.init 2001 (fun _ ->
+  let prices = Bytes.init 2001 (fun _ ->
     let n = !nb in
     nb := next_number n;
-    n mod 10
+    char_of_int (n mod 10)
   ) in
-  let price_diff = Array.init 2000 (fun i -> prices.(i+1) - prices.(i))
+  let price_diff = Bytes.init 2000 (fun i -> (get prices (i+1)) - (get prices i) + 9 |> char_of_int)
   in prices, price_diff
 
 let sequences global_table (prices, prices_diff) =
   let seen = Hashtbl.create 1000 in
+  (* We can represent a numbers between -9 and 9 with 5 bits,
+     so a sequence of four can be encoded in 20 bits, a single integer *)
+  let sequence =
+    (get prices_diff 0 lsl 15) lor
+    (get prices_diff 1 lsl 10) lor
+    (get prices_diff 2 lsl 5) in
+  let sequence = ref sequence in
   Range.iter (fun i ->
-    let seq = Array.sub prices_diff i 4 in
+    let seq = !sequence lor get prices_diff i in
     if not (Hashtbl.mem seen seq) then (
-      hashtbl_incr global_table seq (prices.(i+4));
+      hashtbl_incr global_table seq (get prices (i+1));
       Hashtbl.add seen seq ()
-    ))
-    (Range.upto (2000-3))
+    );
+    sequence := (seq lsl 5) land 0b11111_11111_11111_11111
+    )
+    (Range.interval 3 2000)
 
 let part2 input =
   let global_table = Hashtbl.create 4096 in
